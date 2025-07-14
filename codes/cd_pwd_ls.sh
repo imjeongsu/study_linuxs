e0eac42f6f8272ab68db2bce438d78964aedc0cb
imjeongsu <100193936+imjeongsu@users.noreply.github.com>
2025-07-14 18:00:10 +0900

    과제 완료

---
+++ codes/cd_pwd_ls.sh
@@ -0,0 +1,16 @@
+[im@localhost ~]$ cd Downloads
+[im@localhost Downloads]$ ls
+[im@localhost Downloads]$ pwd
+/home/im/Downloads
+[im@localhost Downloads]$ ls
+[im@localhost Downloads]$ cd ..
+[im@localhost ~]$ cd /home/im/Music
+[im@localhost Music]$ pwd
+/home/im/Music
+[im@localhost Music]$ ls
+[im@localhost Music]$ cd ..
+[im@localhost ~]$ cd /home/im/Pictures
+[im@localhost Pictures]$ pwd
+/home/im/Pictures
+[im@localhost Pictures]$ ls
+[im@localhost Pictures]$ ^C

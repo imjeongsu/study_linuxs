a771906be7949a1bf3a44ab0823fc42f63f290b0
imjeongsu <100193936+imjeongsu@users.noreply.github.com>
2025-07-15 14:27:57 +0900

    done

---
+++ codes/20_controll_file_dir_powershell.sh
@@ -0,0 +1,58 @@
+PS C:\Develops> cd C:\Develops\temp_dir
+PS C:\Develops\temp_dir> pwd
+
+Path
+----
+C:\Develops\temp_dir
+
+
+PS C:\Develops\temp_dir> ls
+PS C:\Develops\temp_dir> mkdir dir_first
+
+
+    디렉터리: C:\Develops\temp_dir
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15   오후 2:22                dir_first
+
+
+PS C:\Develops\temp_dir> ls
+
+
+    디렉터리: C:\Develops\temp_dir
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15   오후 2:22                dir_first
+PS C:\Develops> cd C:\Develops\temp_dir
+PS C:\Develops\temp_dir> pwd
+
+Path
+----
+C:\Develops\temp_dir
+
+
+PS C:\Develops\temp_dir> ls
+PS C:\Develops\temp_dir> mkdir dir_first
+
+
+    디렉터리: C:\Develops\temp_dir
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15   오후 2:22                dir_first
+
+
+PS C:\Develops\temp_dir> ls
+
+
+    디렉터리: C:\Develops\temp_dir
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15   오후 2:22                dir_first
